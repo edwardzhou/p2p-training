6111a05fe1f5bc842c86cb7994f8b23d21c67ac7
edwardzhou <edward.zhq@gmail.com>
2011-02-27 23:24:33 +0800

    Initiailize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P2pTraining::Application.config.secret_token = '66a2e22da4a9468d0585a8253487b819bf29aae00602826387443bb445b2a06e2c9b45749f6c57d12b2a5e2cbd941775d05bf358190a27ba5e8b63d0991a2cf1'
