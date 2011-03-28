1a591d2c5ff9fd731abc2ef5650d96467256c86a
edwardzhou <edward.zhq@gmail.com>
2011-03-28 23:32:53 +0800

    autotest支持drb

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development, :test do
   gem 'webrat'
   gem 'capybara'
   gem 'ZenTest'
+  gem 'spork', '~> 0.9.0.rc'
 end
 
 group :test do
--- spec/models/catalog_spec.rb
+++ spec/models/catalog_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe Catalog do
   #pending "add some examples to (or delete) #{__FILE__}"
-  context "name" do
+  describe "name" do
     before(:all) do
       Factory(:catalog)
     end
 
-    context "should be unique" do
+    describe "should be unique" do
       subject { Factory.build(:catalog) }
       it { should have_at_least(1).error_on(:name) }
     end
