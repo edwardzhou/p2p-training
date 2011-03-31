609a9f8c956bd400a3eaad41cbce348c714c00b9
edwardzhou <edward.zhq@gmail.com>
2011-03-31 21:30:42 +0800

    移除不需要的setup_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,4 @@ module ApplicationHelper
   def show_msg
     render "common/show_msg"
   end
-
-  def setup_user(user)
-    user.tap do |u|
-      u.build_user_detail if u.user_detail.nil?
-    end
-  end
 end
