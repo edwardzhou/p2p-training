80bf3a1ed50052cec9014828eb39d2eee714c1d9
edwardzhou <edward.zhq@gmail.com>
2011-04-18 18:45:05 +0800

    make test passed

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -4,7 +4,8 @@ describe CatalogController do
 
   describe "GET 'view'" do
     it "should be successful" do
-      get 'view'
+      catalog = Factory(:catalog, {:name => 'programming', :enabled => true})
+      get 'view', {:id => catalog.id}
       response.should be_success
     end
   end
