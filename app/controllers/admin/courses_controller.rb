da2a65d8d515c9f540d2e2c05aed75f4ebde7368
edwardzhou <edward.zhq@gmail.com>
2011-04-28 15:56:38 +0800

    添加新增课程场景

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -4,4 +4,17 @@ class Admin::CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(params[:course])
+    if @course.save
+      redirect_to :action => 'index', :notice => t('label.save_success')
+    else
+      render 'new'
+    end
+  end
+
 end
