735328752ebfe5fc75b7d15edd6a7d7a2173b42d
edwardzhou <edward.zhq@gmail.com>
2011-05-03 18:56:29 +0800

    start to add uploaded images browse (not complete)

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,11 +1,17 @@
 class AssetsController < ApplicationController
 
   def upload_image
-    imgUploader = ImageUploader.new
-    imgUploader.cache!( params[:imgFile] )
-    imgUploader.store!
+    img_uploader = ImageUploader.new
+    img_uploader.cache!( params[:imgFile] )
+    img_uploader.store!
 
-    render :inline => {:error => 0, :url => imgUploader.url}.to_json
+    render :inline => {:error => 0, :url => img_uploader.url}.to_json
+  end
+
+  def browse_images
+    root_path = Rails.public_path.join('uploads')
+    #root_url = ActionView::Helpers::AssetTagHelper
+
   end
 
 end
