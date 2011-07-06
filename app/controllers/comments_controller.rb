6cc977826978a3e52102111be347e93ac048c357
edwardzhou <edward.zhq@gmail.com>
2011-07-06 16:24:59 +0800

    implement add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,27 @@
 class CommentsController < ApplicationController
+  before_filter :load_course
   def index
-    @course = Course.find(params[:course_id])
     @comments = @course.comments
+  end
+
+  def new
+    @comment = @course.comments.build
+  end
+
+  def create
+    @comment = Comment.new(params[:comment])
+    @comment.course = @course
+    @comment.user = current_user
+    @comment.save
   end
 
   def destroy
     @course = Course.find(params[:course_id])
     @comment = Comment.find(params[:id])
     @comment.destroy unless @comment.nil?
+  end
+
+  def load_course
+    @course = Course.find(params[:course_id])
   end
 end
