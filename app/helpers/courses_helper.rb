e125261ea38f8c1d584d82b8195e560fb7e35b46
edwardzhou <edward.zhq@gmail.com>
2011-07-13 14:44:42 +0800

    show start_date register_due_date in course detail

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,13 @@
+#encoding: utf-8
 module CoursesHelper
+  def register_due_date(campaign)
+    due_date = campaign.register_due_date
+    due_date ||= "满40人截止"
+  end
+
+  def start_date(campaign)
+    the_date = campaign.start_date
+    the_date ||= "满20人起开课"
+  end
+
 end
