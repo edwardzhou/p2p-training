4ce4864274d42b71be7550d8b8169e5ade47383d
edwardzhou <edward.zhq@gmail.com>
2011-07-18 17:33:04 +0800

    patch for ActiveMerchant::Billing::Integrations::Alipay::Notification.parse

--- config/initializers/alipay.rb
+++ config/initializers/alipay.rb
@@ -7,3 +7,21 @@ require 'active_merchant/billing/integrations/action_view_helper'
 
 ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)
 
+# patch for ActiveMerchant::Billing::Integrations::Alipay::Notification.parse
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    module Integrations #:nodoc:
+      module Alipay
+        class Notification < ActiveMerchant::Billing::Integrations::Notification
+          def parse(post)
+            @raw = post
+            for line in post.split('&')
+              key, value = *line.scan( %r{^(\w+)\=(.*)$} ).flatten
+              params[key] = CGI.unescape(value || '')
+            end
+          end
+        end
+      end
+    end
+  end
+end
