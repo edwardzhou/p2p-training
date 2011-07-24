dd3ebe8be53a4c8ee3888d9971eff075ffa42784
edwardzhou <edward.zhq@gmail.com>
2011-07-25 05:54:55 +0800

    添加课程重复报名检查

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,8 @@ require "id_factory"
 class OrdersController < ApplicationController
   before_filter :authenticate_user!
 
+  include OrdersHelper
+
   def index
     @orders = current_user.orders
   end
@@ -13,6 +15,12 @@ class OrdersController < ApplicationController
     @course = Course.find(params[:course_id])
     @campaigns = @course.active_campaigns
 
+    @already_exists = is_order_exists?(@campaigns.collect{|c| c.id})
+
+    if @already_exists
+      flash[:alert] = t('errors.messages.course_registered')
+    end
+
     @order = Order.new({:user => current_user,
                         :total_amount => @course.discount_price})
   end
@@ -20,14 +28,24 @@ class OrdersController < ApplicationController
   def create
     @course = Course.find(params[:course_id])
     @campaigns = @course.active_campaigns
+
+    @campaign = Campaign.find(params[:selected_campaign])
+
     @order = Order.new({:user => current_user,
                         :total_amount => @course.discount_price})
+
+    if is_order_exists?(@campaign.id)
+      flash[:alert] = 'you have register already.'
+      render 'new'
+      return
+    end
+
     @order.status = '未付款'
     @order.order_code = IdFactory.next_num(:order_id)
     @order_item = @order.order_items.build
-    @order_item.campaign = Campaign.find(params[:selected_campaign])
-    @order_item.price = @course.price
-    @order_item.discount_price = @course.discount_price
+    @order_item.campaign = @campaign
+    @order_item.price = @campaign.price
+    @order_item.discount_price = @campaign.discount_price
     @order_item.qty = 1
     @order_item.calc
     @order.calc
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,7 @@
 module OrdersHelper
+  def is_order_exists?(campaign_ids)
+    current_user.orders.joins(:order_items) \
+      .where(:order_items => {:campaign_id => campaign_ids}) \
+      .where(:status.not_eq => 'cancel').count > 0
+  end
 end
