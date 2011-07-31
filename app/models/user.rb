0ab269f2eb2b916475548d818916101c41de3adf
edwardzhou <edward.zhq@gmail.com>
2011-07-31 22:22:19 +0800

    注册页面改用rails.validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,8 @@ class User < ActiveRecord::Base
 
   include UserDeviseSupport
 
-  validates_presence_of :username, :true_name, :contact_phone
+  validates_presence_of :email, :username, :true_name, :contact_phone
+  validates_presence_of :login, :if => false
   validates_uniqueness_of :username
 
 
@@ -86,58 +87,6 @@ class User < ActiveRecord::Base
   def invited_users
     User.where(:reference_to => self.username)
   end
-
-=begin
-  protected
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    login = conditions.delete(:login)
-    logger.debug("login => #{login}")
-    where(conditions).where(["username = :value OR email = :value", {:value => login}]).first
-  end
-
-  # Attempt to find a user by it's email. If a record is found, send new
-  # password instructions to it. If not user is found, returns a new user
-  # with an email not found error.
-  def self.send_reset_password_instructions(attributes={})
-    recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
-    recoverable.send_reset_password_instructions if recoverable.persisted?
-    recoverable
-  end
-
-  def self.find_recoverable_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
-    (case_insensitive_keys || []).each { |k| attributes[k].try(:downcase!) }
-
-    attributes = attributes.slice(*required_attributes)
-    attributes.delete_if { |key, value| value.blank? }
-
-    if attributes.size == required_attributes.size
-      if attributes.has_key?(:login)
-        login = attributes.delete(:login)
-        record = find_record(login)
-      else
-        record = where(attributes).first
-      end
-    end
-
-    unless record
-      record = new
-
-      required_attributes.each do |key|
-        value = attributes[key]
-        record.send("#{key}=", value)
-        record.errors.add(key, value.present? ? error : :blank)
-      end
-    end
-    record
-  end
-
-  def self.find_record(login)
-    logger.debug("login => #{login}")
-    where(["username = :value OR email = :value", {:value => login}]).first
-  end
-
-=end
 
   private
   def initialize_user_detail
--- app/models/user_devise_support.rb
+++ app/models/user_devise_support.rb
@@ -1,60 +1,59 @@
-module UserDeviseSupportHelper
-  #protected
-  def find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    login = conditions.delete(:login)
-    logger.debug("login => #{login}")
-    where(conditions).where(["username = :value OR email = :value", {:value => login}]).first
-  end
 
-  # Attempt to find a user by it's email. If a record is found, send new
-  # password instructions to it. If not user is found, returns a new user
-  # with an email not found error.
-  def send_reset_password_instructions(attributes={})
-    recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
-    recoverable.send_reset_password_instructions if recoverable.persisted?
-    recoverable
-  end
-
-  def find_recoverable_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
-    (case_insensitive_keys || []).each { |k| attributes[k].try(:downcase!) }
-
-    attributes = attributes.slice(*required_attributes)
-    attributes.delete_if { |key, value| value.blank? }
-
-    if attributes.size == required_attributes.size
-      if attributes.has_key?(:login)
-        login = attributes.delete(:login)
-        record = find_record(login)
-      else
-        record = where(attributes).first
-      end
-    end
-
-    unless record
-      record = new
-
-      required_attributes.each do |key|
-        value = attributes[key]
-        record.send("#{key}=", value)
-        record.errors.add(key, value.present? ? error : :blank)
-      end
-    end
-    record
-  end
-
-  def find_record(login)
-    logger.debug("login => #{login}")
-    where(["username = :value OR email = :value", {:value => login}]).first
-  end
-
-
-end
 
 module UserDeviseSupport
 
-  def self.included(recipient)
-    recipient.extend(UserDeviseSupportHelper)
+  module ClassMethods
+    #protected
+    def find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      login = conditions.delete(:login)
+      logger.debug("login => #{login}")
+      where(conditions).where(["username = :value OR email = :value", {:value => login}]).first
+    end
+
+    # Attempt to find a user by it's email. If a record is found, send new
+    # password instructions to it. If not user is found, returns a new user
+    # with an email not found error.
+    def send_reset_password_instructions(attributes={})
+      recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+      recoverable.send_reset_password_instructions if recoverable.persisted?
+      recoverable
+    end
+
+    def find_recoverable_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
+      (case_insensitive_keys || []).each { |k| attributes[k].try(:downcase!) }
+
+      attributes = attributes.slice(*required_attributes)
+      attributes.delete_if { |key, value| value.blank? }
+
+      if attributes.size == required_attributes.size
+        if attributes.has_key?(:login)
+          login = attributes.delete(:login)
+          record = find_record(login)
+        else
+          record = where(attributes).first
+        end
+      end
+
+      unless record
+        record = new
+
+        required_attributes.each do |key|
+          value = attributes[key]
+          record.send("#{key}=", value)
+          record.errors.add(key, value.present? ? error : :blank)
+        end
+      end
+      record
+    end
+
+    def find_record(login)
+      logger.debug("login => #{login}")
+      where(["username = :value OR email = :value", {:value => login}]).first
+    end
   end
+
+  protected
+  extend ActiveSupport::Concern
 
 end
