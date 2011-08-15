3c4c315af0312c5142f960f75f3518501d90817b
Edward Zhou <edward.zhq@gmail.com>
2011-08-15 21:49:30 +0800

    change system email sender to admin@p2p-training.info

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "edward.zhq@gmail.com"
+  config.mailer_sender = "admin@p2p-training.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
