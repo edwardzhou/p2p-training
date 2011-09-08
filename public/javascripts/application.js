042740c1ad70781b65ac09812d21c672ad6566f5
Edward Zhou <edward.zhq@gmail.com>
2011-09-08 11:23:17 +0800

    hide comments & campaign history

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function hideBlock() {
+    block = $(this).data("target");
+    $("#" + block).hide("blind");
+    if( $(this).data("clean-on-hide") ) {
+        $("#"+block).empty();
+    }
+}
+
+$(document).ready(function(){
+    $('a[hide_block=true]').click(hideBlock)
+});
